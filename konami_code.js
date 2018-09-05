0d6f4f98c278ad23a61fe2f41f459b490d98ee8a
Suheyl Turac <skbsturac@live.com>
2018-09-05 22:37:16 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -19,7 +19,6 @@ let index = 0;
 // This is the function that would be invoked by the event listener.
   document.body.addEventListener('keydown', e => {
     const key = e.key;
-    window.alert("Hurray!");
     if (key === codes[index]) {
       index++;
    
